c1256072b4e2df0d5ef25c20198032415a1b364c
muftiatbakare <bakare.muftiat@gmail.com>
2023-04-11 01:37:59 +0100

    first commit

---
+++ src/ProfileSearchApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Data from "./Components/Data";
+
+const ProfileSearchApp = () => {
+  return (
+    <div>
+      <Data />
+    </div>
+  );
+};
+
+export default ProfileSearchApp;
